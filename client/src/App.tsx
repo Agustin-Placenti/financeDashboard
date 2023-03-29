6ca0a95e3b2886a60f6001e73b1ec67fe03291b9
Agustin-Placenti <agustinplacenti@hotmail.com>
2023-03-29 15:58:28 -0300

    grid dashboard layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,32 @@
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { themeSettings } from "./theme";
+import Navbar from "@/scnes/navbar";
+import Dashboard from "@/scnes/dashboard";
+
 function App() {
-  return <div className="app"></div>
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route
+                path="/predictions"
+                element={<div>predictions page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/expanded-theme.ts
@@ -0,0 +1,12 @@
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+
+// added missing props and number colors to createPalette interfaces
+declare module "@mui/material/styles/" {
+    interface PaletteColor {
+        [key: number]: string;
+    }
+
+    interface Palette {
+        tertiary: PaletteColor;
+    }
+}
